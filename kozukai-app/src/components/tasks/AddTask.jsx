422de8ccf3a5f8725245eada5e13252a2d6cb683
Kailey Waal <15kwaal@gmail.com>
2023-04-08 10:48:46 -0600

    add task

--- kozukai-app/src/components/tasks/AddTask.jsx
+++ kozukai-app/src/components/tasks/AddTask.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
-import { Card, Button, TextField } from "@mui/material";
+import { Card, Button, TextField, Modal, Typography, Box } from "@mui/material";
 import { useAddTaskMutation } from "../../features/tasks";
+import { useTheme } from "@mui/material";
+import { actionColors } from "../../contexts/styles";
+import AddIcon from "@mui/icons-material/Add";
 
 export default function AddTask() {
+  const theme = useTheme();
+
   const [triggerAddTask, resultAddTask] = useAddTaskMutation();
   const [title, setTitle] = useState("");
   const [value, setValue] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
 
   const handleTitleChange = (e) => {
     e.preventDefault();
@@ -21,6 +27,7 @@ export default function AddTask() {
     const task = { title, value };
     triggerAddTask(task);
     clearInput();
+    handleModalClose();
   };
 
   const clearInput = () => {
@@ -28,34 +35,92 @@ export default function AddTask() {
     setValue("");
   };
 
+  const handleModalClose = () => {
+    setModalOpen(false);
+    clearInput();
+  };
+
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
   return (
-    <Card sx={{ display: "flex", alignItems: "center", p: 2, pl: 3, mb: 1 }}>
-      <TextField
-        id="title"
-        label="Task name"
-        type="text"
-        required
-        value={title}
-        onChange={handleTitleChange}
-        sx={{ width: "100%", marginBottom: "10px" }}
-      />
-      <TextField
-        id="value"
-        label="Task value"
-        type="number"
-        required
-        value={value}
-        onChange={handleValueChange}
-        sx={{ width: "100%", marginBottom: "10px" }}
-      />
-      <Button
-        variant="contained"
-        type="submit"
-        onClick={handleAddTask}
-        sx={{ width: "100%", maxWidth: "175px", mt: 2 }}
+    <>
+      <Box sx={{ display: "flex", justifyContent: "flex-end", m: 2 }}>
+        <Button variant="contained" onClick={handleModalOpen}>
+          <AddIcon sx={{ pr: 1 }} />
+          Add Item
+        </Button>
+      </Box>
+      <Modal
+        open={modalOpen}
+        onClose={handleModalClose}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        Add task
-      </Button>
-    </Card>
+        <Card sx={{ maxWidth: "600px", padding: 6 }}>
+          <Typography variant="h3" sx={{ pb: 2 }}>
+            New Task
+          </Typography>
+          <TextField
+            id="title"
+            label="Task name"
+            type="text"
+            required
+            value={title}
+            onChange={handleTitleChange}
+            sx={{ width: "100%", marginBottom: "10px" }}
+          />
+          <TextField
+            id="value"
+            label="Task value"
+            type="number"
+            required
+            value={value}
+            onChange={handleValueChange}
+            sx={{ width: "100%", marginBottom: "10px" }}
+          />
+          <Button
+            variant="outlined"
+            onClick={handleModalClose}
+            sx={{
+              width: "100%",
+              maxWidth: "175px",
+              mt: 2,
+              mr: 1,
+              border: `2px solid ${actionColors.error.main}`,
+              color: actionColors.error.main,
+              backgroundColor: theme.palette.secondary.light,
+              "&:hover": {
+                border: `2px solid ${actionColors.error.dark}`,
+                color: actionColors.error.dark,
+                backgroundColor: theme.palette.secondary.light,
+              },
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            onClick={handleAddTask}
+            sx={{
+              width: "100%",
+              maxWidth: "175px",
+              mt: 2,
+              color: actionColors.information.light,
+              backgroundColor: actionColors.information.main,
+              "&:hover": {
+                backgroundColor: actionColors.information.dark,
+              },
+            }}
+          >
+            Add task
+          </Button>
+        </Card>
+      </Modal>
+    </>
   );
 }
--- kozukai-app/src/contexts/styles.js
+++ kozukai-app/src/contexts/styles.js
@@ -1,5 +1,26 @@
-import { createTheme } from "@mui/system";
-
-export const theme = createTheme({
-  status: {},
-});
+export const actionColors = {
+  success: {
+    light: "#EDF7ED",
+    medium: "#cbe8cb",
+    main: "#4B8E4C",
+    dark: "#1E461F",
+  },
+  warning: {
+    light: "#FFF4E6",
+    medium: "#ffdfb5",
+    main: "#EF7B2C",
+    dark: "#663B01",
+  },
+  error: {
+    light: "#FDEDED",
+    medium: "#f8c1c1",
+    main: "#D84746",
+    dark: "#5F2120",
+  },
+  information: {
+    light: "#E5F6FE",
+    medium: "#b6e6fc",
+    main: "#3397D7",
+    dark: "#024361",
+  },
+};
