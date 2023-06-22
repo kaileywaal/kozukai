1be670bbdeaca0e53710309a090bb7718d1422e2
Kailey Waal <15kwaal@gmail.com>
2023-06-22 14:53:28 -0600

    empty state for no tasks

--- kozukai-app/src/components/tasks/TaskList.jsx
+++ kozukai-app/src/components/tasks/TaskList.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Card } from "@mui/material";
+import { Card, Box, Typography, Link } from "@mui/material";
 import Task from "./Task";
 import { useGetTasksQuery } from "../../features/tasks";
+import { useTheme } from "@emotion/react";
 
 export default function TaskList() {
+  const theme = useTheme();
   const { data: tasks } = useGetTasksQuery();
 
   return (
@@ -12,6 +14,18 @@ export default function TaskList() {
         tasks.map((task) => {
           return <Task task={task} key={task.id} />;
         })}
+      {tasks && tasks.length === 0 && (
+        <>
+          <Box
+            sx={{
+              p: 2,
+              borderBottom: `1px solid ${theme.palette.primary.dark}`,
+            }}
+          >
+            <Typography>Add a task to get started!</Typography>
+          </Box>
+        </>
+      )}
     </Card>
   );
 }
