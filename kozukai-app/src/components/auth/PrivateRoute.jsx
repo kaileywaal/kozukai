fc67c2105e6ad6972d6fc46161f922974aa3c00d
Kailey Waal <15kwaal@gmail.com>
2023-06-22 13:34:46 -0600

    fixed protected route error

--- kozukai-app/src/components/auth/PrivateRoute.jsx
+++ kozukai-app/src/components/auth/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 export default function PrivateRoute({ children }) {
-  const isLoggedIn = localStorage.getItem("AUTH_TOKEN").length > 0;
+  const isLoggedIn = localStorage?.getItem("AUTH_TOKEN")?.length > 0;
 
   return isLoggedIn ? children : <Navigate replace to="/login" />;
 }
